330c14ee9b5b82c2e67ed3c2404b09c466066c8e
Erick Goldberg <erick_goldberg@hotmail.com>
2024-02-12 14:30:09 -0300

    Improving program configuration

---
+++ FinancialGoalManager.API/Extensions/ServiceConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using FinancialGoalManager.Application.Commands.FinancialGoals.RegisterGoal;
+using FinancialGoalManager.Core.Repositories;
+using FinancialGoalManager.Infrastructure.Persistence.Repositories;
+using Microsoft.OpenApi.Models;
+
+namespace FinancialGoalManager.API.Extensions
+{
+    public static class ServiceConfiguration
+    {
+        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.ConfigureDependencyInjection()
+                   .ConfigureOthersServices();
+
+            builder.Services.AddControllers();
+
+            // Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "FinancialGoalManager.API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Erick Goldberg",
+                        Email = "erick_goldberg@hotmail.com",
+                        Url = new Uri("https://github.com/ErickGoldberg")
+                    }
+                });
+            });
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder ConfigureDependencyInjection(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IFinancialGoalRepository, FinancialGoalRepository>();
+            builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+            builder.Services.AddScoped<IReportsRepository, ReportsRepository>();
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder ConfigureOthersServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(typeof(RegisterGoalCommand).Assembly);
+            });
+
+            return builder;
+        }
+    }
+}
--- FinancialGoalManager.API/Program.cs
+++ FinancialGoalManager.API/Program.cs
@@ -1,29 +1,8 @@
-using FinancialGoalManager.Application.Commands.FinancialGoals.RegisterGoal;
-using FinancialGoalManager.Core.Repositories;
-using FinancialGoalManager.Infrastructure.Persistence.Repositories;
+using FinancialGoalManager.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(typeof(RegisterGoalCommand).Assembly);
-});
-
-builder.Services.AddScoped<IFinancialGoalRepository, FinancialGoalRepository>();
-builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-builder.Services.AddScoped<IReportsRepository, ReportsRepository>();
-
-
-
+builder.ConfigureServices();
 
 var app = builder.Build();
 
